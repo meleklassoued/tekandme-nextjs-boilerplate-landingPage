30ca9e13c039b06915f7864f43d7fffaf16396c4
Melek lassoued <melek@tekand.me>
2023-02-15 09:58:04 +0100

    ci: :sparkles: setup nextjs landing page starter

--- Client/Consts.js
+++
@@ -1,6 +0,0 @@
-// Graphql playground end point
-export const BASE_URL_END_POINT = "";
-// Image uploads Base Url
-export const UPLOAD_IMAGE_END_POINT = "";
-// Preprod API URL
-export const PRE_PROD_URL = "";
